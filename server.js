a9b4d6c317b6a30786098e5510bbe17f14ddac73
Victor Arenas <varenas@chiceworx.com>
2022-04-05 21:43:47 -0500

    refactor: :sparkles: Added rooms, clean code and new database users

--- server.js
+++ server.js
@@ -3,48 +3,53 @@ const express = require('express')
 const http = require('http')
 const cors = require('cors')
 const client = require('socket.io')
-const url = 'mongodb://black:135790kp@ds159263.mlab.com:59263/testing'
+const url = 'mongodb://chatuser:mongo@172.20.0.2:27017/mongochat'
 const optionConnect = { useNewUrlParser: true }
 
-//Instancia del express
+// Crea las instancias del servidor express.js
 const app = express()
 const server = http.Server(app)
 const io = client(server)
-
 app.use(cors())
 
-
-
+// Crea el servidor y esta listo para recibir solicitudes
 server.listen(4000, function() {
     console.log('Server start on port 4000!')
+
     //Conectar con la base de datos
     mongo.connect(url, optionConnect, function (err, db) {
         if (err) {
             throw err
         }
-
         console.log('MongoDB Connected...')
+
+        // Obtiene todos los mensajes de la base de datos
+        async function getChatMessages(chat) {
+            const messages = await chat.find().limit(20).sort({ __id: 1 }).toArray()
+            return messages
+        }
 
         //Iniciar la conexion con el socket
         io.on('connection', function (socket) {
-            let chat = db.db('testing').collection('messages')
+
+            // Inicia una sala publica, las salas permiten que varios clientes
+            // se conecten al mismo socket y reciban los mismos eventos
+            socket.join("some room");
+
+            // Crea la variable que manejara la base de datos
+            let chat = db.db('mongochat').collection('messages')
 
             //Funcion para enviar el estado de las peticiones
             sendStatus = function (s) {
-                socket.emit('status', s)
+                io.to("some room").emit('status', s)
             }
 
-            //Obtener los mensajes de la BD
-            chat.find().limit(20).sort({ __id: 1 }).toArray(function (err, res) {
-                if (err) {
-                    throw err
-                }
+            const messages = await getChatMessages(chat)
 
-                //Emitir todos los mensajes
-                socket.emit('output', res)
-            })
+            //Emitir todos los mensajes
+            io.to("some room").emit('output', messages)
 
-            //Canal para eventos de entrada
+            //Canal qeu recibe los datos del cliente
             socket.on('input', function (data) {
                 let name = data.name
                 let message = data.message
@@ -52,11 +57,15 @@ server.listen(4000, function() {
                 if (name == '' || message == '') {
                     sendStatus('Por favor envie datos');
                 } else {
+                    // Inserta el dato en la base de datos
                     chat.insertOne({
                         name: name,
                         message: message,
-                    }, function () {
-                        socket.emit('output', [data])
+                    }, async function () {
+                        // Obtiene todos los mensajes la DB
+                        const allMessages = await getChatMessages(chat)
+                        // Envia todos los mensajes de la DB
+                        io.to("some room").emit('output', allMessages)
 
                         sendStatus({
                             message: 'Mensaje Enviado',
@@ -69,7 +78,7 @@ server.listen(4000, function() {
             //Canal para limpiar todos los mensajes
             socket.on('clear', function (data) {
                 chat.deleteMany({}, function () {
-                    socket.emit('cleared')
+                    io.to("some room").emit('cleared')
                 })
             })
         })
